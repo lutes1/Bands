1f8082acf5ad281a5c5363fdb9b7fae0b2943c7d
Petru Lutenco <lutencopete@gmail.com>
2019-05-09 13:32:38 +0300

    initial commit

---
+++ Client/Bands.Client.iOS/ViewControllers/WelcomeViewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UIKit;
+
+namespace Bands.Views
+{
+    public class WelcomeViewController : UIViewController
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View = new UIView();
+            View.BackgroundColor = UIColor.White;
+        }
+    }
+}
